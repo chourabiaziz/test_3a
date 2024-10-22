bdd2ed13c4d9d7a60d524dc7612f9e60e74c780c
chourabiaziz <chourabiaziz007@gmail.com>
2024-10-22 21:26:42 +0200

    first session

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')] //route pour mettre dans l url  "/home"
+    public function index(): Response // nom fonction avec le type de son retour 
+    {
+        $message  = " Chourabi aziz " ; 
+
+
+        return $this->render('home/index.html.twig'
+                    // tableau associative clé valeur 
+
+                            , [ 
+                                  // clé(a mentionner dans le vue ) => valeur 'affecté pour la vue 
+                                    "msg" => $message
+
+
+                                 ]
+    ); // le retour  " vue => index.html.twig "
+    }
+}
---
+++ src/Entity/Equipe.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipeRepository::class)]
+class Equipe
+{
+    #[ORM\Id] // clé primaire
+    #[ORM\GeneratedValue] // auto increment par 1
+    #[ORM\Column] // champ de DB 
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\OneToMany(mappedBy: 'equipe', targetEntity: Player::class)]
+    private Collection $players;
+
+    public function __construct()
+    {
+        $this->players = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Player>
+     */
+    public function getPlayers(): Collection
+    {
+        return $this->players;
+    }
+
+    public function addPlayer(Player $player): static
+    {
+        if (!$this->players->contains($player)) {
+            $this->players->add($player);
+            $player->setEquipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlayer(Player $player): static
+    {
+        if ($this->players->removeElement($player)) {
+            // set the owning side to null (unless already changed)
+            if ($player->getEquipe() === $this) {
+                $player->setEquipe(null);
+            }
+        }
+
+        return $this;
+    }
+}
